ad8f546e2eae9d575cb2f7c39414da5410090bc3
Gabriel Moraes <gabriel.mmed@gmail.com>
2025-03-05 08:29:31 +0100

    Update database_builder

--- database_builder.py
+++ database_builder.py
@@ -1,14 +1,13 @@
 import yt_dlp
 import os
 
-songs = ["Bad Romance", "Just dance", "Poker Face", "Abracadabra", "G.U.Y.", "Million Reasons", "Shallow", "Always remember you this way", "Telephone", "Born This Way", "Rain on Me"]
 
-def download_youtube_audio(video_name, complement, output_folder="./songs"):
+def download_youtube_audio(song, artist, output_folder="./songs"):
     os.makedirs(output_folder, exist_ok=True)
 
     ydl_opts = {
         "format": "bestaudio/best",
-        "outtmpl": f"{output_folder}/{video_name}.%(ext)s",
+        "outtmpl": f"{output_folder}/{song}.%(ext)s",
         "postprocessors": [{
             "key": "FFmpegExtractAudio",
             "preferredcodec": "mp3",
@@ -17,15 +16,14 @@ def download_youtube_audio(video_name, complement, output_folder="./songs"):
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        print(f"Searching for: {video_name} {complement}")
-        info = ydl.extract_info(f"ytsearch:{video_name} {complement} lyrics", download=True)
+        print(f"Searching for: {song} {artist}")
+        info = ydl.extract_info(f"ytsearch:{song} {artist} lyrics", download=True)
 
         if "entries" in info and len(info["entries"]) > 0:
-            mp3_filename = f"{output_folder}/{video_name}.mp3"
+            mp3_filename = f"{output_folder}/{song}.mp3"
             print(f"Downloaded: {mp3_filename}")
         else:
             print("No results found.")
 
 
-for song in songs:
-    download_youtube_audio(song, 'Lady Gaga')
+
